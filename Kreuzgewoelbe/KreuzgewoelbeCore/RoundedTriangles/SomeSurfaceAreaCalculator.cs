5aa61378ec9a111ae5826cb3598109d268319492
Lars Hollenbach <lohlive@googlemail.com>
2017-02-22 14:44:52 +0100

    Diverse Interfaces implementiert (baut nicht)

---
+++ Kreuzgewoelbe/KreuzgewoelbeCore/RoundedTriangles/SomeSurfaceAreaCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KreuzgewoelbeCore.RoundedTriangles
+{
+	public class SomeSurfaceAreaCalculator : ISurfaceAreaCalculator
+	{
+		// assumes triangles to be unique (no duplicate triangles)
+		public double GetSurface(IEnumerable<ITriangle> triangles)
+		{
+			// rounding errs?
+			return triangles.Aggregate (0.0, 
+				(sum, triangle) =>
+					sum + CalcSurface(triangle)
+				);
+		}
+
+		// this belongs to triangle...
+		private static double CalcSurface(ITriangle triangle)
+		{
+			var deltaB = triangle.B - triangle.A;
+			var deltaC = triangle.C - triangle.A;
+			// surface of the parrelelogram spanned by the vectors
+			// divided by 2 to get the surface of the triangle
+			return Vector3D.CrossProduct (deltaB, deltaC).Length / 2;
+		}
+	}
+}
+
---
+++ Kreuzgewoelbe/KreuzgewoelbeCore/RoundedTriangles/SomeTriangleExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KreuzgewoelbeCore.RoundedTriangles
+{
+	public class SomeTriangleExtractor : ITriangleExtractor
+	{
+		// caution: this expects that ITriangle has
+		// value-semantics for GetHashCode and Equals
+		public IEnumerable<ITriangle> GetTriangles (ITriangulation triangulation)
+		{
+			var ret = new HashSet<ITriangle> ();
+			foreach (var vertex in triangulation.GetVertices()) {
+				foreach (var triangle in vertex.GetAlignedFaces()) {
+					ret.Add (triangle);
+				}
+			}
+
+			return ret;
+		}
+	}
+}
+
