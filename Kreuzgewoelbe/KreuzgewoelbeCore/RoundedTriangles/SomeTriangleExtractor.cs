a70d4d7c9fd8441f27b3fd321bb646817b9c2c5b
Lars Hollenbach <lohlive@googlemail.com>
2017-02-23 10:33:59 +0100

    Equals und GetHashCode zu Triangle und Vertex hinzugefügt.

--- Kreuzgewoelbe/KreuzgewoelbeCore/RoundedTriangles/SomeTriangleExtractor.cs
+++ Kreuzgewoelbe/KreuzgewoelbeCore/RoundedTriangles/SomeTriangleExtractor.cs
@@ -21,4 +21,3 @@ namespace KreuzgewoelbeCore.RoundedTriangles
 		}
 	}
 }
-
--- Kreuzgewoelbe/KreuzgewoelbeCore/RoundedTriangles/Vertex.cs
+++ Kreuzgewoelbe/KreuzgewoelbeCore/RoundedTriangles/Vertex.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace KreuzgewoelbeCore.RoundedTriangles
 {
-    public class Vertex
+	public class Vertex : IEquatable<Vertex>
     {
         public Vector3D Position { get; private set; }
 
@@ -22,5 +22,21 @@ namespace KreuzgewoelbeCore.RoundedTriangles
         {
             return _AlignedFaces;
         }
+
+		public bool Equals(Vertex b)
+		{
+			return b != null &&
+				Position.Equals (b.Position);
+		}
+
+		public override bool Equals(object b)
+		{
+			Equals (b as Vertex);
+		}
+
+		public override int GetHashCode()
+		{
+			return Position.GetHashCode () + this.GetType ().GetHashCode ();
+		}
     }
 }
