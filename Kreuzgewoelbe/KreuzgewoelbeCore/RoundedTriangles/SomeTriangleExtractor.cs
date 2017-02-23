682d96987ac214c12de56565c524573abb3993dc
paul <hahn>
2017-02-23 09:43:29 +0100

    Triangulationrefiner und anpassung von lars sachen
    Wegen geändertem Model waren bei Lars Fehler

--- Kreuzgewoelbe/KreuzgewoelbeCore/RoundedTriangles/SomeTriangleExtractor.cs
+++ Kreuzgewoelbe/KreuzgewoelbeCore/RoundedTriangles/SomeTriangleExtractor.cs
@@ -4,21 +4,23 @@ using System.Linq;
 
 namespace KreuzgewoelbeCore.RoundedTriangles
 {
-	public class SomeTriangleExtractor : ITriangleExtractor
-	{
-		// caution: this expects that ITriangle has
-		// value-semantics for GetHashCode and Equals
-		public IEnumerable<Triangle> GetTriangles(Triangulation<Vertex> triangulation)
-		{
-			var ret = new HashSet<Triangle> ();
-			foreach (var vertex in triangulation.GetVertices()) {
-				foreach (var triangle in vertex.GetAlignedFaces()) {
-					ret.Add (triangle);
-				}
-			}
+    public class SomeTriangleExtractor : ITriangleExtractor
+    {
+        // caution: this expects that ITriangle has
+        // value-semantics for GetHashCode and Equals
+        public IEnumerable<Triangle> GetTriangles<V>(Triangulation<V> triangulation) where V : Vertex
+        {
+            var ret = new HashSet<Triangle>();
+            foreach (var vertex in triangulation.GetVertices())
+            {
+                foreach (var triangle in vertex.GetAlignedFaces())
+                {
+                    ret.Add(triangle);
+                }
+            }
 
-			return ret;
-		}
-	}
+            return ret;
+        }
+    }
 }
 
---
+++ Kreuzgewoelbe/TriangulationRefiner.Library/Cubic/CubicFunctionEdge.cs
@@ -0,0 +1,25 @@
+﻿using KreuzgewoelbeCore;
+using KreuzgewoelbeCore.RoundedTriangles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TriangulationRefiner.Cubic
+{
+    public class CubicFunctionEdge : FunctionEdge
+    {
+        public CubicFunctionEdge(VertexNormal start, VertexNormal end)
+            : base(start, end)
+        { }
+
+        public CubicFunctionEdge(Edge<VertexNormal> toCopy)
+            : this(toCopy.Start, toCopy.End)
+        { }
+
+        protected override Vertex GetNextVertex()
+        {
+            throw new NotImplementedException("todo");
+        }
+    }
+}
