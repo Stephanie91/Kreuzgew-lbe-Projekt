6330a538efba9a9a860be3771c0afe58d8219240
paul <hahn>
2017-03-14 09:07:13 +0100

    test für cubic refiner

---
+++ Kreuzgewoelbe/TriangulationRefinerTest/CubicTriangulationRefiner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangulationRefiner.Cubic;
+using Moq;
+using KreuzgewoelbeCore.RoundedTriangles;
+using KreuzgewoelbeCore;
+
+namespace TriangulationRefinerTest
+{
+    [TestClass]
+    public class CubicTriangulationRefinerTest
+    {
+        [TestMethod]
+        public void CubicTest()
+        {
+            //arrange
+            var fakeEdgeExtractor = new Mock<IEdgeExtractor>();
+
+            Triangle[] triangles = new Triangle[1];
+            VertexNormal vA = new VertexNormal(default(Vector3D), default(Vector3D), triangles);
+            VertexNormal vB = new VertexNormal(default(Vector3D), default(Vector3D), triangles);
+            VertexNormal vC = new VertexNormal(default(Vector3D), default(Vector3D), triangles);
+            triangles[0] = new Triangle(vA, vB, vC);
+
+            var triangulation = new Triangulation<VertexNormal>(new []{vA,vB,vC});
+
+            fakeEdgeExtractor.Setup(e => e.GetEdges(triangulation));
+
+            
+            var refiner = new CubicTriangulationRefiner(fakeEdgeExtractor.Object);
+
+
+            //act
+            var result = refiner.GetRefinedTriangulation(triangulation);
+
+
+            //assert
+
+        }
+    }
+}
