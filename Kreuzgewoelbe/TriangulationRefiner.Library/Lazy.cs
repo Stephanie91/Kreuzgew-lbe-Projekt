d1df2516f084238a6369bdd0e6a2c5a0c1353b77
paul <hahn>
2017-03-14 08:32:04 +0100

    benutzen von.Net 3.5

---
+++ Kreuzgewoelbe/TriangulationRefiner.Library/Lazy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TriangulationRefiner
+{
+    class Lazy<T>
+    {
+        private Func<T> _GetValue;
+        private T _Value;
+
+        public bool IsLoaded { get; private set; }
+
+        public T Value
+        {
+            get
+            {
+                if (!IsLoaded)
+                {
+                    _Value = _GetValue();
+                    IsLoaded = true;
+                }
+                return _Value;
+            }
+        }
+
+        public Lazy(Func<T> getValue)
+        {
+            _GetValue = getValue;
+        }
+    }
+}
